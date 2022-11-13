8fb00e784fcceff34ac889e9df170a85fed068b3
akkiram <114799941+akkiram@users.noreply.github.com>
2022-11-13 21:58:21 +0530

    python
    uploded by python

---
+++ list.py
@@ -0,0 +1,45 @@
+colours = ["black", "blue", "red", "yellow", "00"]
+cars = ["tata", "bmw", "benz", "range rover"]
+numbers = [1,4,6,2,10]
+
+numbers.sort(reverse = True)
+print(numbers)
+
+#newList = [x.upper() for x in cars]
+#print(newList)
+
+
+
+
+# newList = []
+# for i in cars:
+#     if "a" in i:
+#         newList.append(i)
+
+# print(newList)
+
+
+
+#[print(x) for x in cars]
+
+
+# for x in range(len(cars)):
+#     print(cars[x])
+
+
+# x = 0
+# while x < len(cars):
+#     print(cars[x])
+#     x+=1    
+#colours[0:2] = ["yellow", "purple"]
+#print(colours)
+#colours.insert(0,"indigo")
+#colours.append("benz")
+#colours.extend(players)
+# colours.remove("red")
+# colours.pop(1)
+# del colours[0]
+# colours.clear()
+# cars.clear()
+#cars.extend(players)
+#print(cars)
